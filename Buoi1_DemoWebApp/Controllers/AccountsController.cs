5728e9d3df5f431ea7c304f8737b1c303f9bf519
Hoang Son <son.lyhoang2014@gmail.com>
2024-05-14 20:20:02 +0700

    Add project files.

---
+++ Buoi1_DemoWebApp/Controllers/AccountsController.cs
@@ -0,0 +1,27 @@
+﻿using Buoi1_DemoWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Buoi1_DemoWebApp.Controllers
+{
+    public class AccountsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            List<Account> accounts = new List<Account>
+            {
+                new Account { Id = 1, UserName = "admin", Password = "admin" },
+                new Account { Id = 2, UserName = "sonlh", Password = "son123" },
+                new Account { Id = 3, UserName = "customer", Password = "123456" }
+            };
+
+            ViewBag.abcdef = accounts;
+
+            return View();
+        }
+    }
+}
---
+++ Buoi1_DemoWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Buoi1_DemoWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
